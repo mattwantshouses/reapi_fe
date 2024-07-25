2cbd4f88e4b64c303a2fb9f1b1ef7c63250385f4
Matt <mattwantshouses@gmail.com>
2024-07-25 18:24:07 -0400

    Added a Material UI AppBar and Toolbar to the Layout component.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,3 +21,26 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 };
 
 export default Layout;
+import React from 'react';
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Property Search App</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <main>{children}</main>
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
